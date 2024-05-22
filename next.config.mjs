11d9ce4961a81a72c567c86c00b6f595315a57bd
daichi2mori <daichi2mori@gmail.com>
2024-05-22 16:49:21 +0900

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: "/code-shot",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
