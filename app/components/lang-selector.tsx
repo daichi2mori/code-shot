f869de96001a5c4e0a551f17f1852a05f669aaff
daichi2mori <daichi2mori@gmail.com>
2024-09-15 15:22:49 +0900

    feat: add search function when selecting language

--- app/components/lang-selector.tsx
+++ app/components/lang-selector.tsx
@@ -1,35 +1,72 @@
 "use client";
 
+import { useState } from "react";
 import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { Button } from "@/components/ui/button"
+import { Check, ChevronsUpDown } from "lucide-react"
 import { bundledLanguagesInfo } from "shiki";
 import { useLangSelector } from "../store/lang-store";
+import { cn } from "@/lib/utils"
 
 const LangSelector = () => {
+  const [open, setOpen] = useState(false)
   const lang = useLangSelector((state) => state.lang)
   const setLang = useLangSelector((state) => state.setLang)
 
   return (
-    <Select defaultValue={lang} onValueChange={(value) => setLang(value)}>
-      <SelectTrigger className="w-36 rounded-full">
-        <SelectValue />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          {bundledLanguagesInfo.map((item) => (
-            <SelectItem key={item.id} value={item.id}>
-              {item.name}
-            </SelectItem>
-          ))}
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+    <Popover open={open} onOpenChange={setOpen}>
+    <PopoverTrigger asChild>
+      <Button
+        variant="outline"
+        role="combobox"
+        aria-expanded={open}
+        className="w-[150px] justify-between rounded-full"
+      >
+        {lang}
+        <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+      </Button>
+    </PopoverTrigger>
+    <PopoverContent className="w-[200px] p-0">
+      <Command>
+        <CommandInput placeholder="Search lang..." />
+        <CommandList>
+          <CommandEmpty>No lang found.</CommandEmpty>
+          <CommandGroup>
+            {bundledLanguagesInfo.map((item) => (
+              <CommandItem
+                key={item.id}
+                value={item.id}
+                onSelect={(currentValue) => {
+                  setLang(currentValue)
+                  setOpen(false)
+                }}
+              >
+                <Check
+                  className={cn(
+                    "mr-2 h-4 w-4",
+                    lang === item.name ? "opacity-100" : "opacity-0"
+                  )}
+                />
+                {item.name}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </CommandList>
+      </Command>
+    </PopoverContent>
+  </Popover>
   );
 };
 
